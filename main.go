5b59dd3d25324b0c5efc30ca7ecb2c060681f967
reanthony <reanthony@crimson.ua.edu>
2022-12-02 10:57:50 -0600

    Day Two

--- main.go
+++ main.go
@@ -5,14 +5,36 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 var sum = 0
 var maxSum = 0
 
 func main() {
+	// Day One
+	maxSum, err := DayOne()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(maxSum)
 
-	file, err := os.Open("input.txt")
+	//Day Two
+	totalPoints, err := DayTwo()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(totalPoints)
+}
+
+func DayTwo() (points int, err error) {
+	var priceMap map[string]int = make(map[string]int, 9)
+
+	priceMap["A"], priceMap["X"] = 1, 1
+	priceMap["B"], priceMap["Y"] = 2, 2
+	priceMap["C"], priceMap["Z"] = 3, 3
+
+	file, err := os.Open("./Input/DayTwo/input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -20,26 +42,88 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		text := scanner.Text()
-		DayOne(text)
+		currLine := scanner.Text()
+		currLine = strings.TrimSpace(currLine)
+		opponent := priceMap[currLine[0:1]]
+		me := priceMap[currLine[2:3]]
+		pointsAwarded, err := calculateDayTwoPoints(opponent, me)
+		if err != nil {
+			panic(err)
+		}
+		points += pointsAwarded
 	}
-
-	fmt.Println(maxSum)
+	return points, nil
 }
 
-func DayOne(currLine string) {
-	if currLine == "" {
-		if sum > maxSum {
-			maxSum = sum
-		}
-		sum = 0
-		return
+func calculateDayTwoPoints(opponent int, me int) (pointsAwsrded int, err error) {
+	//if draw
+	if opponent == me {
+		pointsAwsrded += me + 3
+		return pointsAwsrded, nil
 	}
+	// declare winner
+	won := declareWinnerDayTwo(opponent, me)
+	if won {
+		pointsAwsrded += 6 + me
+	} else {
+		pointsAwsrded += me
+	}
+	return pointsAwsrded, nil
+}
 
-	line, err := strconv.ParseInt(currLine, 10, 64)
+func declareWinnerDayTwo(opponent int, me int) bool {
+	if me == 1 {
+		if opponent == 2 {
+			return false
+		}
+		if opponent == 3 {
+			return true
+		}
+	}
+	if me == 2 {
+		if opponent == 1 {
+			return true
+		}
+		if opponent == 3 {
+			return false
+		}
+
+	}
+	if me == 3 {
+		if opponent == 1 {
+			return false
+		}
+		if opponent == 2 {
+			return true
+		}
+	}
+	return false
+}
+
+func DayOne() (maxSum int, err error) {
+	file, err := os.Open("./Input/DayOne/input.txt")
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 
-	sum += int(line)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		currLine := scanner.Text()
+		if currLine == "" {
+			if sum > maxSum {
+				maxSum = sum
+			}
+			sum = 0
+			return
+		}
+
+		line, err := strconv.ParseInt(currLine, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		sum += int(line)
+	}
+	return maxSum, nil
 }
