a7c21f8a9e1deb40d42eb3a3f4ced20f99ee8696
reanthony <reanthony@crimson.ua.edu>
2022-12-07 15:13:11 -0600

    Day four part one

--- main.go
+++ main.go
@@ -25,6 +25,14 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(totalPoints)
+
+	// Day four
+	result, err := DayFour()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
 }
 
 func DayTwo() (points int, err error) {
@@ -126,4 +134,102 @@ func DayOne() (maxSum int, err error) {
 		sum += int(line)
 	}
 	return maxSum, nil
+}
+
+func DayFour() (totalSize int, err error) {
+	file, err := os.Open("./Input/DayFour/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	result := 0
+
+	for scanner.Scan() {
+		currLine := scanner.Text()
+		parts := strings.Split(currLine, ",")
+		first := parts[0]
+		second := parts[1]
+		firstParts := strings.Split(first, "-")
+		firstBegin := firstParts[0]
+		firstEnd := firstParts[1]
+		secondParts := strings.Split(second, "-")
+		secondBegin := secondParts[0]
+		secondEnd := secondParts[1]
+		firstArry, secondArry := makeArrays(firstBegin, firstEnd, secondBegin, secondEnd)
+		result = DayFourHelper(firstArry, secondArry, result)
+	}
+
+	return result, nil
+
+}
+
+func makeArrays(firstBegin string, firstEnd string, secondBegin string, secondEnd string) (firstArray []int, secondArray []int) {
+	firstBeginInt, err := strconv.Atoi(firstBegin)
+	if err != nil {
+		panic(err)
+	}
+	firstEndInt, err := strconv.Atoi(firstEnd)
+	if err != nil {
+		panic(err)
+	}
+	secondBeginInt, err := strconv.Atoi(secondBegin)
+	if err != nil {
+		panic(err)
+	}
+	secondEndInt, err := strconv.Atoi(secondEnd)
+	if err != nil {
+		panic(err)
+	}
+
+	indx := 0
+	firstArray = make([]int, (firstEndInt - firstBeginInt + 1))
+	for i := firstBeginInt; i <= firstEndInt; i++ {
+		firstArray[indx] = i
+		indx++
+	}
+
+	indx = 0
+	secondArray = make([]int, (secondEndInt - secondBeginInt + 1))
+	for i := secondBeginInt; i <= secondEndInt; i++ {
+		secondArray[indx] = i
+		indx++
+	}
+
+	return firstArray, secondArray
+
+}
+
+func DayFourHelper(first []int, second []int, result int) int {
+
+	//Check first is contained in second
+	if isSubset(first, second) {
+		result++
+		return result
+	}
+	if isSubset(second, first) {
+		result++
+	}
+
+	//Check second is contained in first
+	return result
+}
+
+func isSubset(first []int, second []int) bool {
+	var myMap map[int]bool = make(map[int]bool, len(first))
+	for _, f := range first {
+		myMap[f] = true
+	}
+
+	for _, s := range second {
+		if _, ok := myMap[s]; !ok {
+			return false
+		}
+	}
+
+	// second is fully contained in first
+	// second is a full subset of first
+	return true
 }
